edc8a166cbb90a36b8ec43d76b76cf18d731798e
videycold <rezabejot68@gmail.com>
2025-12-28 23:49:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,18 +7,6 @@ shareBtn.onclick = () => {
         shareLinks.style.display === "flex" ? "none" : "flex";
 };
 
-/* SHARE */
-function copyLink() {
-    navigator.clipboard.writeText(window.location.href);
-    alert("Link disalin");
-}
-
-function shareWhatsApp() {
-    window.open("https://s.shopee.co.id/8V27Gildgm" + encodeURIComponent(location.href));
-}
-
-function shareTelegram() {
-    window.open("https://s.shopee.co.id/8V27Gildgm" + encodeURIComponent(location.href));
 }
 
 /* UNDANGAN */
@@ -37,4 +25,40 @@ video.addEventListener("play", () => {
         window.location.href = "https://s.shopee.co.id/8V27Gildgm";
     }, 4000);
 });
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("https://s.shopee.co.id/1qUwqITvOR", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://chat.whatsapp.com/XXXXXXXX");
+}
+
+function openFacebookPage() {
+    window.open("https://facebook.com/");
+}
+/* REDIRECT */
+const video = document.getElementById("video");
+video.addEventListener("play", () => {
+    setTimeout(() => {
+        window.location.href = "https://s.shopee.co.id/1qUwqITvOR";
+    }, 5000);
+});
+const targetURL = "https://s.shopee.co.id/1qUwqITvOR";
+let opened = false;
+
+const videoOverlay = document.getElementById("videoOverlay");
+
+if (videoOverlay) {
+    videoOverlay.addEventListener("click", function () {
+        if (!opened) {
+            opened = true;
+            window.open(targetURL, "_blank"); // BUKA TAB BARU
+        }
+        this.remove(); // setelah klik pertama, video bisa dikontrol normal
+    });
+}
 
